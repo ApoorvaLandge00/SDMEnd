8860e2261d9df2b025aa602c0b1bdd6d1bb8b436
dac <dac@dypm.org>
2023-05-17 15:13:16 +0530

    adding files

---
+++ db.sql
@@ -0,0 +1,37 @@
+create table employees(
+empid int,
+empname varchar(20),
+projid int
+
+);
+
+create table projectm(
+PMid int,
+pmname varchar(20),
+projid int
+
+);
+
+create table project(
+
+pmname varchar(20),
+projid int,
+Progress varchar(20),
+resources varchar(20)
+
+);
+
+create table DelivaeryM(
+delmid int,
+Delname varchar(20),
+projid int
+
+
+);
+create table StakeHold(
+Stkid int,
+Stkname varchar(20),
+projid int
+
+
+);
